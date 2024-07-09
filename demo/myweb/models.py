a9d4e1d0486970146e750f7799fdbfe72c265f2e
syufree <github>
2024-07-09 20:03:03 +0800

    111

---
+++ demo/myweb/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+
